6296c11b452b600aa8ba0a553c6cd91776720fe8
Andrew Ribeiro <andtankian@gmail.com>
2024-03-31 12:19:29 -0300

    feat: Add functionality to run tests for a single test

--- lua/nxtest.lua
+++ lua/nxtest.lua
@@ -15,8 +15,27 @@ end
 
 local function runTestForFile()
 	local projectName = utils.getNxProjectName()
-	local command = projectName .. ":test " .. '"' .. utils.getFilePath() .. '"' .. " --watch"
+	local command = projectName .. ":test --runTestsByPath " .. '"' .. utils.getFilePath() .. '"' .. " --watch"
 	runCmd(command)
+end
+
+local function runTestForSingle()
+	local projectName = utils.getNxProjectName()
+
+	local line = vim.api.nvim_get_current_line()
+	local _, _, testName = string.find(line, "^%s*%a+%(['\"](.+)['\"]")
+
+	if testName ~= nil then
+		local command = projectName
+			.. ":test --runTestsByPath "
+			.. '"'
+			.. utils.getFilePath()
+			.. '"'
+			.. ' --testNamePattern=\'"'
+			.. testName
+			.. '"\' --watch'
+		runCmd(command)
+	end
 end
 
 local M = {}
@@ -24,6 +43,7 @@ local M = {}
 M.setup = function()
 	usercmd("NxTest", runTestForProject, { nargs = 0 })
 	usercmd("NxTestFile", runTestForFile, { nargs = 0 })
+	usercmd("NxTestSingle", runTestForSingle, { nargs = 0 })
 end
 
 return M
