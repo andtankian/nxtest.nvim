29d1bbf703e6dba5201f94ce092143a2bc80e637
Andrew Ribeiro <andtankian@gmail.com>
2024-04-05 12:18:09 -0300

    fix: nx project

--- lua/nxtest.lua
+++ lua/nxtest.lua
@@ -3,67 +3,68 @@ local utils = require("utils")
 local cmd = vim.cmd
 local user_cmd = vim.api.nvim_create_user_command
 
-local function run_cmd(command_table)
-  local command = table.concat(command_table, "")
-  cmd("vsplit | terminal cd " .. vim.loop.cwd() .. "&& pnpm nx run " .. command)
+local function run_cmd(command_table, opts)
+	if opts ~= nil then
+		table.insert(command_table, " " .. opts.args)
+	end
+
+	local command = table.concat(command_table, "")
+	cmd("vsplit | terminal cd " .. vim.loop.cwd() .. "&& pnpm nx run " .. command)
 end
 
-local function run_test_for_project()
-  local project_name = utils.get_nx_project_name()
-  local command = project_name .. ":test" .. " --watch"
-  run_cmd(command)
+local function run_test_for_project(opts)
+	local command_table = {}
+
+	local project_name = utils.get_nx_project_name()
+
+	table.insert(command_table, project_name)
+	table.insert(command_table, ":test")
+
+	run_cmd(command_table, opts)
 end
 
 local function run_test_for_file(opts)
-  local command_table = {}
+	local command_table = {}
 
-  local project_name = utils.get_nx_project_name()
+	local project_name = utils.get_nx_project_name()
 
-  table.insert(command_table, project_name)
-  table.insert(command_table, ":test")
-  table.insert(command_table, " --skip-nx-cache")
-  table.insert(command_table, " --runTestsByPath ")
-  table.insert(command_table, utils.get_file_path())
-  table.insert(command_table, " --watch")
-  if opts ~= nil then
-    table.insert(command_table, " " .. opts.args)
-  end
+	table.insert(command_table, project_name)
+	table.insert(command_table, ":test")
+	table.insert(command_table, " --runTestsByPath ")
+	table.insert(command_table, utils.get_file_path())
+	table.insert(command_table, " --watch")
 
-  run_cmd(command_table)
+	run_cmd(command_table, opts)
 end
 
-local function run_test_for_single()
-  local command_table = {}
+local function run_test_for_single(opts)
+	local command_table = {}
 
-  local project_name = utils.get_nx_project_name()
+	local project_name = utils.get_nx_project_name()
 
-  local line = vim.api.nvim_get_current_line()
-  local _, _, test_name = string.find(line, "^%s*%a+%(['\"](.+)['\"]")
+	local line = vim.api.nvim_get_current_line()
+	local _, _, test_name = string.find(line, "^%s*%a+%(['\"](.+)['\"]")
 
-  if test_name ~= nil then
-    table.insert(command_table, project_name)
-    table.insert(command_table, ":test")
-    table.insert(command_table, " --skip-nx-cache")
-    table.insert(command_table, " --runTestsByPath ")
-    table.insert(command_table, utils.get_file_path())
-    table.insert(command_table, " --testNamePattern='\"")
-    table.insert(command_table, test_name)
-    table.insert(command_table, "$\"'")
-    table.insert(command_table, " --watch")
-    if opts ~= nil then
-      table.insert(command_table, " " .. opts.args)
-    end
+	if test_name ~= nil then
+		table.insert(command_table, project_name)
+		table.insert(command_table, ":test")
+		table.insert(command_table, ' --runTestsByPath "')
+		table.insert(command_table, utils.get_file_path())
+		table.insert(command_table, '" --testNamePattern=\'"')
+		table.insert(command_table, test_name)
+		table.insert(command_table, "\"$'")
+		table.insert(command_table, " --watch")
 
-    run_cmd(command_table)
-  end
+		run_cmd(command_table, opts)
+	end
 end
 
 local M = {}
 
 M.setup = function()
-  user_cmd("NxTest", run_test_for_project, { nargs = 0 })
-  user_cmd("NxTestFile", run_test_for_file, { nargs = "*" })
-  user_cmd("NxTestSingle", run_test_for_single, { nargs = 0 })
+	user_cmd("NxTest", run_test_for_project, { nargs = "*" })
+	user_cmd("NxTestFile", run_test_for_file, { nargs = "*" })
+	user_cmd("NxTestSingle", run_test_for_single, { nargs = "*" })
 end
 
 return M
